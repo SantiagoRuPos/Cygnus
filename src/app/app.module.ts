10430b536f199a6c3addd5bf3293491fede5d8a7
SantiagoRuPos <santiagorupoz@gmail.com>
2023-04-06 13:26:18 -0500

    Cuerpos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './view/home/home.component';
+import { FooterComponent } from './view/footer/footer.component';
+import { MenuComponent } from './view/menu/menu.component';
+import { ServicioTecnicoComponent } from './view/servicio-tecnico/servicio-tecnico.component';
+import { ProjectsComponent } from './view/projects/projects.component';
+import { Error404Component } from './emistakes/HTTP/error404/error404.component';
+import { Error501Component } from './emistakes/HTTP/error501/error501.component';
+import { FilterPipe } from './pipes/filter.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    FooterComponent,
+    MenuComponent,
+    ServicioTecnicoComponent,
+    ProjectsComponent,
+    Error404Component,
+    Error501Component,
+    FilterPipe
   ],
   imports: [
     BrowserModule,
