7d78eb3168fad425e62f98da249c61810c6a9700
SantiagoRuPos <santiagorupoz@gmail.com>
2023-04-23 15:00:54 -0500

    Ultimos toques

--- src/app/view/projects/projects.component.ts
+++ src/app/view/projects/projects.component.ts
@@ -9,18 +9,18 @@ export class ProjectsComponent implements OnInit {
 
   filterPost="";
 Proyecto =[
-  {"nombre":"Proyecto LoT",
-   "Descripcion":"Proyecto plataforma LoT",
-    "Url":"/proyecto1/index.html"},
+  {"nombre":"Proyecto IoT",
+   "Descripcion":"Proyecto plataforma IoT",
+    "Url":"http://cygnus.uniajc.edu.co/proyecto1/index.html"},
   {"nombre":"Proyecto 2",
   "Descripcion":"Proyecto 2",
-   "Url":"/proyecto2/index.html"},
+   "Url":"http://cygnus.uniajc.edu.co/proyecto2/index.html"},
   {"nombre":"Proyecto Santiago Ruiz",
   "Descripcion":"Proyecto Santiago Ruiz",
-   "Url":"/webruiz/index.html"},
+   "Url":"http://cygnus.uniajc.edu.co/webruiz/index.html"},
   {"nombre":"Proyecto LR",
   "Descripcion":"Proyecto de laboratorio remoto de fisica",
-   "Url":"/proyectolr/index.html"},
+   "Url":"http://cygnus.uniajc.edu.co/proyectolr/index.html"},
 ]
 ngOnInit(): void {
 this.Mensahe_Projectos();
